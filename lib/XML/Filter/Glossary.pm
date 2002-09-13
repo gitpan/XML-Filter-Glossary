7158a44634c9f23f65b0a9a937dac3ded02ba9c4
Aaron Straup Cope <ascope@cpan.org>
2002-09-13 03:01:31 +0000

    Import of ASCOPE/XML-Filter-Glossary-0.2 from CPAN.
    gitpan-cpan-distribution: XML-Filter-Glossary
    gitpan-cpan-version:      0.2
    gitpan-cpan-path:         ASCOPE/XML-Filter-Glossary-0.2.tar.gz
    gitpan-cpan-author:       ASCOPE
    gitpan-cpan-maturity:     released

--- lib/XML/Filter/Glossary.pm
+++ lib/XML/Filter/Glossary.pm
@@ -28,9 +28,13 @@ This package is modelled after the UserLand glossary system where words, or phra
 
 Currently only one type of lookup table is supported : a well-formed XBEL bookmarks file. Support for other kinds of lookup tables may be added at a later date.
 
-Keywords are flagged as being any word, or words, between double quotes which are then looked up in the glossary. If no match is found, the text is left unaltered.
+=head1 KEYWORDS
 
-If a match is located, the result is then parsed with Robert Cameron's REX shallow parsing regular expressions. Chunks of balanced markup are then re-inserted into the SAX stream via I<XML::Filter::Merger>. Anything else, including markup not deemed well-formed, is added as character data.
+Keywords are flagged as being any word, or words, between double quotes which are then looked up in the glossary. Alternately, you may specify keyword phrases with singleton elements that are the property of a user-defined namespace.
+
+If no match is found, the text is left unaltered.
+
+If a match is located, the result is then parsed with Robert Cameron's REX shallow parsing regular expressions. Chunks of balanced markup are then re-inserted into the SAX stream via I<XML::Filter::Merger>. Anything else, including markup not determined to be well-formed, is added as character data.
 
 =cut
 
@@ -43,7 +47,7 @@ use XML::SAX::ParserFactory;
 use vars qw( @ISA );
 @ISA = qw( XML::Filter::Merger );
 
-$XML::Filter::Glossary::VERSION = '0.1';
+$XML::Filter::Glossary::VERSION = '0.2';
 
 # REX/Perl 1.0 
 # Robert D. Cameron "REX: XML Shallow Parsing with Regular Expressions",
@@ -103,9 +107,161 @@ sub set_glossary {
   $self->{'__glossary'} = $_[0];
 }
 
+=head2 $pkg->register_namespace()
+
+Register data to allow the filter to recognize specific tags as containing data to be used for keyword lookup.
+
+Valid arguments are 
+
+=over
+
+=item *
+
+B<hash reference>
+
+=over
+
+=item *
+
+I<Prefix>
+
+String. 
+
+The prefix for your glossary namespace.
+
+=item *
+
+I<NamespaceURI>
+
+String. 
+
+The URI for your glossary namespace.
+
+=item *
+
+I<KeywordAttr>
+
+String.
+
+Default value is "id"
+
+=back
+
+ # Use <g:keyword /> syntax
+ $glossary->register_namespace({
+				Prefix       => "g",
+				NamespaceURI => "http://www.aaronland.net/glossary"
+			       });
+
+ # Use <g:keyword phrase = "keyword with spaces" /> syntax
+ $glossary->register_namespace({
+				Prefix       => "g",
+				NamespaceURI => "http://www.aaronland.net/glossary",
+                                KeywordAttr  => "phrase",
+			       });
+
+=item *
+
+B<zero>
+
+ # Toggle back to default double-quote syntax
+ $glossary->register_namespace(0);
+
+=back
+
+=cut
+
+sub register_namespace {
+  my $self = shift;
+  my $ns   = shift;
+
+  if (! $ns) {
+    $self->{'__nsaware'}   = 0;
+    $self->{'__prefix'}    = undef;
+    $self->{'__namespace'} = undef;
+
+    return 1;
+  }
+
+  if (ref($ns) ne "HASH") {
+    print STDERR "Namespace data must be passed as a hash reference.\n";
+    return 0;
+  }
+
+  if (($ns->{Prefix}) && 
+      ($ns->{NamespaceURI})) {
+
+    $self->{'__nsaware'}   = 1;
+    $self->{'__prefix'}    = $ns->{Prefix};
+    $self->{'__namespace'} = $ns->{NamespaceURI};
+    $self->{'__kwattr'}    = $ns->{KeywordAttr} || "id";
+    return 1;
+  }
+}
+
+sub start_prefix_mapping {
+  my $self = shift;
+  my $data = shift;
+
+  return if (($data->{'Prefix'} eq $self->{'__prefix'}) &&
+	     ($data->{'NamespaceURI'} eq $self->{'__namespace'}));
+
+  $self->SUPER::start_prefix_mapping($data);
+  return 1;
+}
+
+sub start_element {
+  my $self = shift;
+  my $data = shift;
+
+  unless (($self->{'__nsaware'}) &&
+	  ($self->{'__prefix'} eq $data->{'Prefix'})) {
+
+    $self->_stripnamespace($data) if (! $self->{'__bangns'});
+
+    $self->SUPER::start_element($data);
+    return 1;
+  }
+
+  #
+
+  my $keyword = $data->{Attributes}->{'{}'.$self->{'__kwattr'}}->{'Value'} || $data->{'LocalName'};
+
+  if (($keyword) && (my $result = $self->lookup_keyword($keyword))) {
+
+    $self->process_result(\$result);
+    return 1;
+  }
+
+  #
+
+  $self->SUPER::characters({Data=>$keyword});
+  return 1;
+}
+
+sub end_element {
+  my $self = shift;
+  my $data = shift;
+
+  unless (($self->{'__nsaware'}) &&
+	  ($self->{'__prefix'} eq $data->{'Prefix'})) {
+
+    $self->SUPER::end_element($data);
+    return 1;
+  }
+
+}
+
 sub characters    {
   my $self = shift;
   my $data = shift;
+
+  if ($self->{'__nsaware'}) {
+    $self->SUPER::characters($data);
+    return 1;
+  }
+
+  #
 
   while (not $data->{Data} =~ m/\G\z/gc) {
 
@@ -118,22 +274,7 @@ sub characters    {
 
    if ($keyword) {
 
-     if (! exists $self->{'__cache'}{$keyword}) {
-
-       if (! $self->{'__lookup'}) {
-	 my $lookup = join("::",__PACKAGE__,"XBEL");
-	 eval "require $lookup;";
-
-	 $self->{'__lookup'} = $lookup->new();
-	 $self->{'__parser'} = XML::SAX::ParserFactory->parser(Handler=>$self->{'__lookup'});
-       }
-
-       $self->{'__lookup'}->set_keyword($keyword);
-       $self->{'__parser'}->parse_uri($self->{'__glossary'});
-       $self->{'__cache'}{$keyword} = $self->{'__lookup'}->result();
-     }
-
-     if (my $result = $self->{'__cache'}{$keyword}) {
+     if (my $result = $self->lookup_keyword($keyword)) {
 
        $self->SUPER::characters({Data=>"$text "});
        $self->process_result(\$result);
@@ -152,6 +293,28 @@ sub characters    {
   }
 
   return 1;
+}
+
+sub lookup_keyword {
+  my $self    = shift;
+  my $keyword = shift;
+
+  if (! exists $self->{'__cache'}{$keyword}) {
+
+    if (! $self->{'__lookup'}) {
+      my $lookup = join("::",__PACKAGE__,"XBEL");
+      eval "require $lookup;";
+
+      $self->{'__lookup'} = $lookup->new();
+      $self->{'__parser'} = XML::SAX::ParserFactory->parser(Handler=>$self->{'__lookup'});
+    }
+
+    $self->{'__lookup'}->set_keyword($keyword);
+    $self->{'__parser'}->parse_uri($self->{'__glossary'});
+    $self->{'__cache'}{$keyword} = $self->{'__lookup'}->result();
+  }
+
+  return $self->{'__cache'}{$keyword};
 }
 
 sub process_result {
@@ -229,13 +392,27 @@ sub process_result {
   return 1;
 }
 
+sub _stripnamespace {
+  my $self = shift;
+  my $data = shift;
+
+  foreach my $ns (keys %{$data->{Attributes}}) {
+    if ($ns eq "{http://www.w3.org/2000/xmlns/}".$self->{'__prefix'}) {
+      delete $data->{Attributes}{$ns};
+      $self->{'__bangns'} = 1;
+      last;
+    }
+  }
+
+}
+
 =head1 VERSION
 
-0.1
+0.2
 
 =head1 DATE
 
-September 10, 2002
+September 12, 2002
 
 =head1 AUTHOR
 
@@ -279,7 +456,13 @@ L<XML::Filter::Merger>
 
 =head1 BUGS
 
+=over
+
+=item *
+
 Certainly, not outside the realm of possibility.
+
+=back
 
 =head1 LICENSE
 
